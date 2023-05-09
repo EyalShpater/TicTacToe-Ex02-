91af33aae0124a15f1789edbc7bdf0d9356305d2
matanhayon <matanhayon@gmail.com>
2023-05-09 12:16:25 +0300

    quick and dirty 2

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -105,17 +105,6 @@ namespace TicTacToe
             return sign != null;
         }
 
-        public Board(int i_Size)
-        {
-            m_Board = new char[i_Size, i_Size];
-        }
-
-        public void Mark(int i_X, int i_Y, char i_Sign)
-        {
-
-        }
-
-        private is
     }
 
 }
